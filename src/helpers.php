840d5f3a9d74b2f70da35f40fea359c972b1d93a
Rick <rgreer4@users.noreply.github.com>
2023-05-16 16:59:35 -0500

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -36,8 +36,15 @@ function ddumper_text()
  */
 function dumper()
 {
+    $output = '';
+
     // arguments passed to this function
     $args = func_get_args();
+
+    if(count($args) < 2)
+        {
+        throw new \Horizom\VarDumper\VarDumperException('Insufficient arguments.');
+        }
 
     // options (operators) gathered by the expression parser;
     // this variable gets passed as reference to getInputExpressions(), which will store the operators in it
@@ -45,11 +52,13 @@ function dumper()
 
     // names of the arguments that were passed to this function
     $expressions = VarDumper::getInputExpressions($options);
-    $capture = in_array('@', $options, true);
+
+    //$capture = in_array('@', $options, true);
+    $capture = array_shift($args);
 
     // something went wrong while trying to parse the source expressions?
     // if so, silently ignore this part and leave out the expression info
-    if (func_num_args() !== count($expressions)) {
+    if (func_num_args()-1 !== count($expressions)) {
         $expressions = null;
     }
 
@@ -63,17 +72,13 @@ function dumper()
 
     $ref = new VarDumper($format);
 
-    if ($capture) {
-        ob_start();
-    }
-
     foreach ($args as $index => $arg) {
-        $ref->query($arg, $expressions ? $expressions[$index] : null);
+        $output .= $ref->query($arg, $expressions ? $expressions[$index] : null);
     }
 
     // return the results if this function was called with the error suppression operator
-    if ($capture) {
-        return ob_get_clean();
+    if (!$capture) {
+        return $output;
     }
 
     // stop the script if this function was called with the bitwise not operator
@@ -81,6 +86,8 @@ function dumper()
         print '</body></html>';
         exit(0);
     }
+
+    return $output;
 }
 
 /**
